9bf44fa8caa310373a4b61b8ab15f4b79956f3d3
luisjuarez099 <luisjuarezcc9@gmail.com>
2024-07-30 00:26:37 -0600

    conexion

--- Controller/escenario.controler.php
+++ Controller/escenario.controler.php
@@ -1,21 +1,30 @@
 <?php
-require 'conexion.php';
-$objeto = new Conexion();
-$conexion = $objeto->Conectar();
 
-$titulo = $_POST['titulo'];
-$descripcion = $_POST['descripcion'];
-// $pasos = $_POST[''];
-// $TesterAsignado = $_POST[''];
-// $archivo = $_POST[''];
+require_once 'conexion.php';
 
-$insertar = "INSERT INTO escenario VALUES ('$titulo','$descripcion')";
-
-$query = mysqli_query($link, $insertar);
-
-if($query){
-
-    
+class ControllerEscenario{
+    public function escenarioController($modescenario){
+        $tabla = 'escenario';
+        $conn = Conexion::conectar();
+        $stmt = $conn->prepare("INSERT INTO $tabla (nombre) VALUES (:nombre)");
+        $stmt->bindParam(':nombre', $modescenario->getNombre(), PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $val = $stmt->fetchAll();
+        try {
+            if ($val == null || $val == '' || $val == [] || $val == false) {
+                throw new Exception('No se encontro el escenario');
+            }
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+        for ($i = 0; $i < count($val); $i++) {
+            if ($val[$i]['nombre'] == $modescenario->getNombre()) {
+                header('Location: View/home-crear.php');
+                exit();
+            }
+        }
+    }
 }
 
 ?>
